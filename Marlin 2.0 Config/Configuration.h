5d74f25a49dbdd54078ce42f0e89136dd075aa37
Steve <pessimism56@gmail.com>
2019-12-28 09:50:42 -0500

    correct extruder steps/mm

--- Marlin 2.0 Config/Configuration.h
+++ Marlin 2.0 Config/Configuration.h
@@ -718,7 +718,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 1600, 90 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 1600, 103 }
 
 /**
  * Default Max Feed Rate (mm/s)
