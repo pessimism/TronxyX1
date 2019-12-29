92fd3872bbf7cf2ae0b9c5c8e6bf051fa1db02f8
Steve <pessimism56@gmail.com>
2019-12-29 09:56:20 -0500

    update for loss of bed area due to tensioner

--- Marlin 2.0 Config/Configuration.h
+++ Marlin 2.0 Config/Configuration.h
@@ -1059,7 +1059,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 150
-#define Y_BED_SIZE 150
+#define Y_BED_SIZE 125
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -4.25
