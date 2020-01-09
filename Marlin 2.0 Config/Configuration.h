947c7fd3a892e0dd15f64f79dc10ad662fcfcc51
Steve <pessimism56@gmail.com>
2020-01-09 06:28:28 -0500

    set bed back to 150x150

--- Marlin 2.0 Config/Configuration.h
+++ Marlin 2.0 Config/Configuration.h
@@ -1059,7 +1059,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 150
-#define Y_BED_SIZE 120
+#define Y_BED_SIZE 150
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -4.25
