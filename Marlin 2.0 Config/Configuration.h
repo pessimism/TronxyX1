6257b8a9592e7fb0b98a958c3eca6ae9a2496de2
Steve Trotman <pessimism56@gmail.com>
2020-02-10 14:29:21 -0500

    extruder steps per m for PLA w leoway extruder

--- Marlin 2.0 Config/Configuration.h
+++ Marlin 2.0 Config/Configuration.h
@@ -718,7 +718,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 1600, 180 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 1600, 152 }
 
 /**
  * Default Max Feed Rate (mm/s)
