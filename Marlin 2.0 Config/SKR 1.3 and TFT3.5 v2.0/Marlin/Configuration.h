f5769514d25a2d355d6ba8704bf7dd0c69d97529
Steve Trotman <pessimism56@gmail.com>
2020-04-04 12:21:46 -0400

    adjust bed offset

--- Marlin 2.0 Config/SKR 1.3 and TFT3.5 v2.0/Marlin/Configuration.h
+++ Marlin 2.0 Config/SKR 1.3 and TFT3.5 v2.0/Marlin/Configuration.h
@@ -1096,8 +1096,8 @@
 #define Y_BED_SIZE 150
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -4.25
-#define Y_MIN_POS -14.25
+#define X_MIN_POS -4.0
+#define Y_MIN_POS -20.0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
