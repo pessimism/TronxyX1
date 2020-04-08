7563143c36d41b6d172722d0e7e994a34c8a975f
Steve Trotman <pessimism56@gmail.com>
2020-04-08 07:54:11 -0400

    set sane default for linear advance and tune esteps

--- Marlin 2.0 Config/SKR 1.3 and TFT3.5 v2.0/Marlin/Configuration.h
+++ Marlin 2.0 Config/SKR 1.3 and TFT3.5 v2.0/Marlin/Configuration.h
@@ -730,7 +730,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 3200, 304 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 3200, 316 }
 
 /**
  * Default Max Feed Rate (mm/s)
