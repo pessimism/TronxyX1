c39b4f5763f6ca4a325eb017e759ec99ae10494e
Steve Trotman <pessimism56@gmail.com>
2020-06-07 18:45:10 -0400

    update config

--- Marlin 2.0 Config/SKR 1.3 and Reprap 12864/Marlin/Configuration.h
+++ Marlin 2.0 Config/SKR 1.3 and Reprap 12864/Marlin/Configuration.h
@@ -777,7 +777,7 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-//#define CLASSIC_JERK
+#define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
   #define DEFAULT_XJERK 20.0
   #define DEFAULT_YJERK 20.0
@@ -812,7 +812,7 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-#define S_CURVE_ACCELERATION
+//#define S_CURVE_ACCELERATION
 
 //===========================================================================
 //============================= Z Probe Options =============================
